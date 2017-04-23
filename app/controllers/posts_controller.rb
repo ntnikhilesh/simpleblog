ef1bb2b01c42f1fc7063c840221e78efd1feeb77
nikhil <ntnikhilesh@gmail.com>
2017-04-23 20:37:43 +0530

    store data in db after click of submit button and print val on diff page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,25 @@
 class PostsController < ApplicationController
+    #def keyword used for defining function
+    # @varable name for defining varialble
     def index
 
+    end
+     def show
+        @post=Post.find(params[:id])
     end
     def new
 
     end
     def create
-        render plain: params[:post].inspect
+        #on click of submit button on view page this function will execute
+        #render plain: params[:post].inspect  # print object on browser
+        @post=Post.new(post_params)  #Post is the model name
+        @post.save #save data to db
+        redirect_to @post #then reditect to show page
     end
+
+    #get all form data and save into private variable
+    private def post_params
+        params.require(:post).permit(:title,:body)
+        end
 end
